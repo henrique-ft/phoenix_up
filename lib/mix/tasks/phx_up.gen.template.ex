4873b27d5b7fb5da821c39a0549af849dbd88061
henrique.teixeira <henrique.teixeira@locaweb.com.br>
2020-02-22 01:16:55 -0300

    [ajust-template] Add template generator

---
+++ lib/mix/tasks/phx_up.gen.template.ex
@@ -0,0 +1,44 @@
+defmodule Mix.Tasks.PhxUp.Gen.Template do
+  @shortdoc false
+  @moduledoc false
+
+  alias PhoenixUp.Mix.Inflector
+  use Mix.Task
+
+  @doc false
+  def run(io_puts \\ true, args) do
+    if io_puts == true do
+      IO.puts("""
+           _             __ __ _____
+       ___| |_ _ _      |  |  |  _  |
+      | . |   |_'_|     |  |  |   __|
+      |  _|_|_|_,_|_____|_____|__|
+      |_|         |_____|            .gen.template
+      """)
+    end
+
+    IO.puts("... Preparing template")
+
+    context = get_context(args)
+
+    create_template(context)
+
+    IO.puts("")
+  end
+
+  defp create_template(context) do
+    copy_template(
+      "template.eex",
+      "lib/#{context[:web_path]}/templates/#{context[:path]}.html.eex",
+      context: context
+    )
+  end
+
+  defp copy_template(name, final_path, opts) do
+    Path.join(:code.priv_dir(:phoenix_up), "templates/phx_up.gen.template/#{name}")
+    |> Mix.Generator.copy_template(final_path, opts)
+  end
+
+  defp get_context([module]), do: Inflector.call(module)
+  defp get_context([]), do: raise(RuntimeError, "Invalid module name")
+end
